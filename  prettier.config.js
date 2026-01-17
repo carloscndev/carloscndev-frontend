9d1b88c88095dbdf0f34d76f533151cc0b116d94
carloscncdev <carloscndev@gmail.com>
2026-01-16 22:17:49 -0600

    feat(astro): install and upate dependencies

---  prettier.config.js
+++  prettier.config.js
@@ -1,9 +1,10 @@
-module.exports = {
+export default {
   semi: true,
   singleQuote: false,
   trailingComma: "es5",
   tabWidth: 2,
   printWidth: 80,
   endOfLine: "auto",
+  astroAllowShorthand: true,
   plugins: ["prettier-plugin-astro"],
 };
