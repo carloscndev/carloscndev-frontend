3fdeb8213021f4fd9e60bc487a2c3491499e8ea1
carloscncdev <carloscndev@gmail.com>
2026-06-15 20:46:01 -0600

    chore: adding mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import mermaid from 'astro-mermaid';
 
 export default defineConfig({
   site: "https://carloscndev.com",
   output: "static",
   integrations: [
     sitemap(),
+    mermaid()
   ],
   vite: {
     plugins: [tailwindcss()],
